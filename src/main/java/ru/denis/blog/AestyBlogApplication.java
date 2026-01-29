38a539c2667453512c23f90446d4a75ee5a8740f
aesty1 <kadirovdenis123@gmail.com>
2026-01-29 16:05:30 +0500

    first commit

---
+++ src/main/java/ru/denis/blog/AestyBlogApplication.java
@@ -0,0 +1,13 @@
+package ru.denis.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AestyBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AestyBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/denis/blog/AestyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.denis.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AestyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
